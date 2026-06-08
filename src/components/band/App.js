de2d9d3193bfea6b94d062e4b27b1ef7cb3f43b9
tripanboi-png/ <tripanboi@gmail.com>
2026-06-09 06:28:18 +0700

    Update App.js

--- src/components/band/App.js
+++ src/components/band/App.js
@@ -225,7 +225,7 @@ function Band({ isMobile, maxSpeed = 50, minSpeed = 10 }) {
 
   return (
     <>
-      <group position={isMobile ? [1.6, 3.3, 0] : [3, 4, 0]}>
+      <group position={isMobile ? [0.8, 2.45, 0] : [3, 4, 0]}>
         <RigidBody ref={fixed} {...segmentProps} type="fixed" />
         <RigidBody position={[0.5, 0, 0]} ref={j1} {...segmentProps}><BallCollider args={[0.1]} /></RigidBody>
         <RigidBody position={[1, 0, 0]} ref={j2} {...segmentProps}><BallCollider args={[0.1]} /></RigidBody>
